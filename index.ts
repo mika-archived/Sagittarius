e594e8b53b1690357313f15af7ef6b04de9f9a83
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-12-22 19:32:10 +0900

    Hello, TypeScript

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as electron from 'electron'; 
+import {remote} from 'electron'; 
+const app = remote.require('app');
+const BrowserWindow = remote.require('browser-window');
+const dialog = remote.require('dialog');
+
+function hello(){
+    var options = {
+        title: 'ダイアログのタイトル',
+        type: 'info',
+        buttons: ['OK', 'Cancel'],
+        message: 'メッセージ',
+        detail: 'hello'
+    };
+    var win = BrowserWindow.getFocusedWindow();
+    dialog.showMessageBox(win, options);
+}
