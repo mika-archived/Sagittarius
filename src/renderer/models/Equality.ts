d79bcc12c4dde364578d538b690668f88476e6ff
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-03-13 22:27:54 +0900

    Test passing.

--- src/renderer/models/Equality.ts
+++ src/renderer/models/Equality.ts
@@ -8,18 +8,22 @@ export function equalsToArray(array1: any[], array2: any[]): boolean {
       return false;
     }
   }
-  
   return true;
 }
 
 export function equalsTo(obj1: any, obj2: any): boolean {
   if(obj1 instanceof Object) {
     for(var propName in obj1) {
-      if(obj1[propName] != obj2[propName]) {
+      if(!equalsTo(obj1[propName], obj2[propName])) {
         return false;
       }
+    }
+  } else if (obj1 instanceof Array) {
+    if(!equalsToArray(obj1, obj2)) {
+      return false;
     }
   } else {
     return obj1 === obj2;
   }
+  return true;
 }
