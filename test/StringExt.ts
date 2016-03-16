20a5539c343be18ebcc3585ab3a5c6d3d590fea7
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-03-16 23:05:01 +0900

    Add test cases.

---
+++ test/StringExt.ts
@@ -0,0 +1,21 @@
+/// <reference path="../src/typings/tsd.d.ts" />
+
+import "../src/renderer/extensions/StringExt";
+
+describe("StringExt", () => {
+  it("string.Format - case 1", () => {
+    "Hello, world".format("Hello").should.equal("Hello, world");
+  })
+  
+  it("string.Format - case 2", () => {
+    "Hello, {0}".format("world").should.equal("Hello, world");
+  })
+  
+  it("string.Format - case 3", () => {
+    "{0}, {1}".format("Hello", "world").should.equal("Hello, world");
+  })
+  
+  it("string.Format - case 4", () => {
+    "{0}, {1}{2}".format("Hello", "world").should.equal("Hello, world{2}");
+  });
+});
