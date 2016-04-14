2c181a4ffa1379a3c4f976b621f7c75512aedda1
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-04-14 20:41:53 +0900

    Update for title bar

--- src/renderer/containers/AppFrame.tsx
+++ src/renderer/containers/AppFrame.tsx
@@ -16,6 +16,8 @@ import {Me} from '../models/Me';
 import {Message} from '../models/Message';
 import {Room} from '../models/Room';
 
+const BrowserWindow = require('electron').remote.BrowserWindow;
+
 /* inner */
 interface RoomMembers {
   members: Contact[];
@@ -85,6 +87,7 @@ class AppFrame extends React.Component<AppFrameProps, {}> {
         .single(w => w.roomId == this.props.selectChatRoom);
       members = this.props.roomMembers[room.roomId].members;
       messages = this.props.roomMessages[room.roomId].messages;
+      BrowserWindow.getFocusedWindow().setTitle("Sagittarius - {0}".format(room.name));
     }
     return (
       <div>
